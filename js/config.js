3ef956535afae30188f701f05b0f652abab00e15
Tobias Paczian <paczian@mcs.anl.gov>
2017-07-11 08:03:22 -0500

    updated config

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var RetinaConfig = {
     // server urls
-    "shock_url": "http://localhost:9000",
-    "auth_url": "http://localhost:9000/cgi-bin/oAuth.cgi"
+    "shock_url": "https://144.221.76.245/seqcenter-app",
+    "auth_url": "https://144.221.76.245/seqcenter-app/cgi-bin/oAuth.cgi"
 };
