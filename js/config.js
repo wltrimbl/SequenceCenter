b86bab134ce51fbd5e7f41cf44688c97cab6372b
Tobias Paczian <paczian@mcs.anl.gov>
2017-08-16 06:21:59 -0500

    set URLs to https

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var RetinaConfig = {
     // server urls
-    "shock_url": "http://sequencing.bio.anl.gov",
+    "shock_url": "https://sequencing.bio.anl.gov",
     "shock_preauth": "http://shock.metagenomics.anl.gov/preauth",
-    "auth_url": "http://sequencing.bio.anl.gov/cgi-bin/oAuth.cgi"
+    "auth_url": "https://sequencing.bio.anl.gov/cgi-bin/oAuth.cgi"
 };
