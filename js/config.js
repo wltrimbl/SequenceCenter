c86876de23fc900d8650b74f9b4cad7ebb44f368
Travis Harrison <guumby@gmail.com>
2018-04-25 14:51:30 -0500

    fix shock url

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var RetinaConfig = {
     // server urls
-    "shock_url": "https://sequencing.bio.anl.gov",
+    "shock_url": "https://shock.mg-rast.org",
     "shock_preauth": "http://shock.metagenomics.anl.gov/preauth",
     "auth_url": "https://sequencing.bio.anl.gov/cgi-bin/oAuth.cgi"
 };
