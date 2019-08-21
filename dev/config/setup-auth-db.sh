29acb3e9cff8ed2a1d0dcbe29d56d3daee6b62e3
Will Trimble <trimble@anl.gov>
2019-08-21 09:42:54 -0500

    Remove -T option for mysql, not supported

--- dev/config/setup-auth-db.sh
+++ dev/config/setup-auth-db.sh
@@ -4,11 +4,11 @@ result=1
 
 while [ $result -ne 0 ]
 do   
- # mysql -u authService --password=authServicePassword --host db -T -D DemoAppUsers  < /tmp/dbsetup.demo.mysql
+ # mysql -u authService --password=authServicePassword --host db -D DemoAppUsers  < /tmp/dbsetup.demo.mysql
  sleep 5
  echo Initializing database `date`
- echo "mysql -u $MYSQL_USER --password=$MYSQL_PASSWORD --host $MYSQL_HOST -T -D $MYSQL_DATABASE  < /tmp/auth-db.mysql"
- mysql -u $MYSQL_USER --password=$MYSQL_PASSWORD --host $MYSQL_HOST -T -D $MYSQL_DATABASE  < /tmp/auth-db.mysql
+ echo "mysql -u $MYSQL_USER --password=$MYSQL_PASSWORD --host $MYSQL_HOST -D $MYSQL_DATABASE  < /tmp/auth-db.mysql"
+ mysql -u $MYSQL_USER --password=$MYSQL_PASSWORD --host $MYSQL_HOST -D $MYSQL_DATABASE  < /tmp/auth-db.mysql
  result=$?
 done
 
